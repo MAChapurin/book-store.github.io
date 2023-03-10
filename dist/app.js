7b0cc0f9d4ff0b9216e6b9e7e0b16e1d25bad725
Michael <machapurin@icloud.com>
2023-03-10 21:53:48 +0300

    change app.js

--- dist/app.js
+++ dist/app.js
@@ -1203,9 +1203,9 @@
                     class="search__input"
                     value="${this.state.searchQuery ? this.state.searchQuery : ''}"
                     />
-                    <img src = "/static/search.svg" alt="search icon"/>
+                    <img src = "static/search.svg" alt="search icon"/>
         </div>
-        <button><img src="/static/search-white.svg"/></button>
+        <button><img src="static/search-white.svg"/></button>
         `;
             this.el.querySelector('button').addEventListener('click', this.search.bind(this));
             this.el.querySelector('input').addEventListener('keydown', (event) => {
